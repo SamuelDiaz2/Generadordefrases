5397cfdf5a6af3509246e349fb1f7a31e0d32d76
Juan1624 <juanestebanarias164@gmail.com>
2026-02-07 15:05:54 -0500

    estilos de la app

--- GeneradorFrases/src/main.js
+++ GeneradorFrases/src/main.js
@@ -4,13 +4,6 @@ import viteLogo from '/vite.svg'
 import { setupFrases } from './generador.js'
 
 document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
 
     <h1>Generador de frases 🚀</h1>
 
@@ -21,9 +14,6 @@ document.querySelector('#app').innerHTML = `
       <p id="frase"></p>
     </div>
 
-    <p class="read-the-docs">
-      Hecho con Vite + JavaScript
-    </p>
   </div>
 `
 
